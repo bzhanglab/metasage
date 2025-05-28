0946ed285e0a8924f93e8c3261268b6759e2202b
John Elizarraras <elizarrj27@gmail.com>
2025-05-28 15:01:36 -0500

    add support for static site building

--- src/routes/blog/[slug]/+page.js
+++
@@ -1,8 +0,0 @@
-export async function load({ params }) {
-  const post = await import(`../../../content/blog/${params.slug}.svx`);
-
-  return {
-    content: post.default,
-    metadata: post.metadata
-  };
-}
