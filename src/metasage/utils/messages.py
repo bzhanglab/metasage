76580ecda25e187fba739e59a9d0538a1df09e33
BCMwang <113642505+BCMwang@users.noreply.github.com>
2026-01-07 17:00:54 -0600

    Delete src/metasage directory

--- src/metasage/utils/messages.py
+++
@@ -1,9 +0,0 @@
-from rich import print
-
-
-def print_error(msg: str) -> None:
-    print(f"[red bold]ERROR:[/red bold] [red]{msg}[/red]")
-
-
-def print_info(msg: str) -> None:
-    print(f"[blue]{msg}[/blue]")
